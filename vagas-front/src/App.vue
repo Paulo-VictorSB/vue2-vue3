b9ca3c26f3454a8ab4bf4178e170bde65cad5147
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-09-26 12:23:33 -0300

    Customizando o header

--- vagas-front/src/App.vue
+++ vagas-front/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <Header/>
     <MainContent/>
   </div>
@@ -17,14 +17,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
