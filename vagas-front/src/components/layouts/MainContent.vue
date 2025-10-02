1a53d5592d9ae2d64373b6f2f39bb8d80fd34b81
Paulo Victor <paulovdbarbosa@gmail.com>
2025-10-02 20:02:32 -0300

    Aprendendo como guardar uma instancia de um componente em cache no vuejs

--- vagas-front/src/components/layouts/MainContent.vue
+++ vagas-front/src/components/layouts/MainContent.vue
@@ -8,7 +8,9 @@
     <button @click="conteudo = 'HomeView' ">Home</button>
     <button @click="conteudo = 'PublicarVaga'">Publicar Vaga</button>
     
-    <component :is="conteudo" />
+    <keep-alive>
+      <component :is="conteudo" />
+    </keep-alive>
 
     <!-- <HomeView/>
     <publicarVaga/> -->
@@ -34,43 +36,43 @@ export default {
     titulo: 'Componente conteudo',
     conteudo: 'HomeView'
   }),
-  beforeCreate() {
-    console.log("Antes de criar")
-  },
-  // Muito usando antes da criação (obvio) de um componente, mas no ambito de validar se o usuario está autênticado e afins.
-  created() {
-    console.log(this.teste)
-  },
-  // Quando o componente foi criado, você utiliza esse lifecycle para manipular os dados. NOTA: isso só é feito após toda a injeção de dependencias e reatividades.
+  // beforeCreate() {
+  //   console.log("Antes de criar")
+  // },
+  // // Muito usando antes da criação (obvio) de um componente, mas no ambito de validar se o usuario está autênticado e afins.
+  // created() {
+  //   console.log(this.teste)
+  // },
+  // // Quando o componente foi criado, você utiliza esse lifecycle para manipular os dados. NOTA: isso só é feito após toda a injeção de dependencias e reatividades.
   
-  // Antes de consolidar a instância do vue com um componenete ele procura um EL(do vue2) e compila o html nele. E se tiver um template ele implementa os dados nesse componente.
+  // // Antes de consolidar a instância do vue com um componenete ele procura um EL(do vue2) e compila o html nele. E se tiver um template ele implementa os dados nesse componente.
 
-  // Porém, antes de compilar o template e jogar no DOM final, nós temos a opção de tomar uma ação. e ela é feita através do lifecycle beforeMount()
-  beforeMount() {
-    console.log('Antes de montar o componente')  
-  },
-  // Esse lifecycle é bastante utilizado para fazer uma requisição http para o backend e retornar os dados para recuperar dados para compor o nosso template.
+  // // Porém, antes de compilar o template e jogar no DOM final, nós temos a opção de tomar uma ação. e ela é feita através do lifecycle beforeMount()
+  // beforeMount() {
+  //   console.log('Antes de montar o componente')  
+  // },
+  // // Esse lifecycle é bastante utilizado para fazer uma requisição http para o backend e retornar os dados para recuperar dados para compor o nosso template.
 
-  // Quando o vue monta de fato o template já compilado dentro de um elemento html ele aciona o lifecycle mounted() indicando assim que o componente foi montado.]
-  mounted() {
-    console.log('Componente Montado')
-  },
+  // // Quando o vue monta de fato o template já compilado dentro de um elemento html ele aciona o lifecycle mounted() indicando assim que o componente foi montado.]
+  // mounted() {
+  //   console.log('Componente Montado')
+  // },
 
-  // beforeUpdate e updated são gatilhos disparados no componente antes e após uma atualização do mesmo.
-  beforeUpdate() {
-    console.log('Antes de atualizar')
-  },
-  updated() {
-    console.log('Atualizado')
-  },
+  // // beforeUpdate e updated são gatilhos disparados no componente antes e após uma atualização do mesmo.
+  // beforeUpdate() {
+  //   console.log('Antes de atualizar')
+  // },
+  // updated() {
+  //   console.log('Atualizado')
+  // },
 
-  // beforeUnmout e unmounted são gatilhso disparados no componente antes e após a desmontagem do componente
-  beforeUnmount() {
-    console.log("Componente MainContent antes de ser destruido")
-  },
-  unmounted() {
-    console.log("Componente MainContent destruido")
-  }
+  // // beforeUnmout e unmounted são gatilhso disparados no componente antes e após a desmontagem do componente
+  // beforeUnmount() {
+  //   console.log("Componente MainContent antes de ser destruido")
+  // },
+  // unmounted() {
+  //   console.log("Componente MainContent destruido")
+  // }
 }
 
 </script>
--- vagas-front/src/components/views/Home.vue
+++ vagas-front/src/components/views/Home.vue
@@ -6,7 +6,21 @@
 
 <script>
 export default {
-    name: "HomeView"
+    name: "HomeView",
+    activated() {
+      console.log("Componente ativado")
+    },
+    deactivated() {
+      console.log("Componente desativado")
+    },
+    beforeUnmount() {
+      console.log("Antes de desmontar/destruir")
+    },
+    unmounted() {
+      console.log("Desmontado/destruido")
+    }
+
+    // Caso você precise que a instancia do componente permaneca ativa você pode usar o lifecycle activated e deactivated, basicamente esse componente precisa ficar em cash. Melhor para performace.
 }
 </script>
 
