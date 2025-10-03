ca22da828be9c4e919bccfd542c54e0780f8e0da
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-10-03 13:50:33 -0300

    Passando os dados para o componente vaga

--- vagas-front/src/components/utils/Vaga.vue
+++ vagas-front/src/components/utils/Vaga.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="card">
-    <div class="card-header bg-dark text-white">[TÍTULO DA VAGA]</div>
-    <div class="card-body">[INFORMAÇÃO DA VAGA]</div>
+    <div class="card-header bg-dark text-white">{{ titulo }}</div>
+    <div class="card-body">{{ descricao }}</div>
     <div class="card-footer">
-      <small class="text-muted">Sálario: R$6.000,00 | Modalidade: Remoto | Tipo: CLT</small>
+      <small class="text-muted">
+        Sálario: R${{ salario }} | Modalidade: {{ modalidade }} |
+        Tipo: {{ tipo }} | Publicação: {{ publicacao }}
+      </small>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "VagaVue"
+  name: "VagaVue",
+  props: {
+    titulo: String,
+    descricao: String,
+    salario: String,
+    modalidade: String,
+    tipo: String,
+    publicacao: String,
+  },
 };
 </script>
 
